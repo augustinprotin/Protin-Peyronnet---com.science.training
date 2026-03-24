937588f314e21781d077be48a73441f6da26b466
Augustin <augugu78@gmail.com>
2026-03-24 08:52:30 +0100

    creation du cahrgement des donnees

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import pandas as pd
+
+
 def main():
     return 0
+
+def chargement_des_donnes (nom_fichier_csv):
+    data=pd.read_csv(nom_fichier_csv, sep=",", encoding="utf8")
+    print ("données chargées : ")
+    print(data.head)
+
+
+chargement_des_donnes("Data/student-por.csv")
+
